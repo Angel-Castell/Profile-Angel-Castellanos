1a877665baeca758a44496ab938ded4a5973c626
Angel Castellanos <angel.castellanos@netlogistik.com>
2022-11-01 21:22:27 -0600

    First project added to portfolio

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,36 @@
 function update(numero){
 	for (var i = 0; i < 5; i++) {
-		document.getElementsByClassName('state')[i].classList.remove("active");
+		document.getElementsByClassName('state')[i].classList.remove("active-menu-item");
 	}
 
-	document.getElementsByClassName('state')[numero].classList.add("active");
+	document.getElementsByClassName('state')[numero].classList.add("active-menu-item");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active-dot";
 }
